93f3401bc6885a0ef82de0a1ddb87a6ae1ffbbb4
nilrudragupta-boop <nilrudragupta@gmail.com>
2026-04-25 21:26:40 +0530

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,22 @@
+const RELEASE_DATE = "02 April 2026";
+const DEVELOPER_EMAIL = "nilrudragupta@gmail.com";
+const CURRENT_APP_VERSION = "2.3.8";
+const DOWNLOAD_URL = "https://drive.google.com/uc?export=download&id=1anVmf6e0gZRDdWuhSX1Ur_MihtIlAbK3";
+const RELEASE_DATA = {
+    newFeatures: [
+        "Implemented & Integrated Bill Of Materials(BOM) feature to streamline production planning and cost analysis.",
+        "Implemented a Production Management Module to manage production processes within the system.",
+        "Implemented Data Analysis (BI) to track business overview.",
+        "Implemented a new feature to allow users to set up and manage recurring transactions, enabling automated scheduling of regular payments and income entries within the system.",
+    ],
+
+    improvements: [
+        "Improved Item Module to integrate with BOM & Production Management Module for better navigation and business overview.",
+        "Redesigned the Invoice PDF interface to improve readability and overall user experience.",
+        "Enhanced the performance of the Banking Simulator Module for smoother and more responsive user experience.",
+    ],
+
+    bugfixes: [
+        "Fixed some minor bugs to improve overall system stability."
+    ]
+};
